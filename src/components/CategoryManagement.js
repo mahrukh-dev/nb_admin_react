380fe65bd0e8cc3a47729e9f76a5d5d111d16a5d
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2025-12-01 21:10:05 +0500

    fix

--- src/components/CategoryManagement.js
+++ src/components/CategoryManagement.js
@@ -8,7 +8,8 @@ const CategoryManagement = () => {
   const [editingCategory, setEditingCategory] = useState(null);
   const [formData, setFormData] = useState({ name: '', description: '' });
 
- const API_BASE_URL = 'https://nbbackend-production.up.railway.app/api';
+
+ const API_BASE_URL = 'https://nbbackend-production-8c50.up.railway.app/api';
 
 
   // Fetch all categories
